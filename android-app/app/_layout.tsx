d8abb1231da6d9749cf2e56141d31f2e0836ce96
Maxdeoro <maxdeoro@gmail.com>
2025-09-17 21:21:01 +0300

    layout & Stack routing

---
+++ android-app/app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Slot, Stack, Tabs } from "expo-router";
+import { Text } from 'react-native';
+import { Colors } from "../shared/tokens";
+
+export default function  RootLayout() {
+    // return <Tabs />;
+    return <Stack screenOptions={{
+        statusBarBackgroundColor: Colors.black,
+        contentStyle: {
+            backgroundColor: Colors.black,
+        }
+    }}>
+        <Stack.Screen name='index' />
+        <Stack.Screen name='restore'
+                      options={{
+                        presentation:'modal',
+                      }} />
+    </Stack>;
+};
--- android-app/app/restore.tsx
+++ android-app/app/restore.tsx
@@ -1,8 +1,11 @@
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function Restore() {
     return <View>
+        {/* <Stack.Screen options={{
+            title: 'Restore password',
+        }}/> */}
         <Link href={'/'}>
             <Text>LOGIN</Text>
         </Link>
