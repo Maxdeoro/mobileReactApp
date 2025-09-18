d3903cc1d52439712f9e6b622517e48beddcc5be
Maxdeoro <maxdeoro@gmail.com>
2025-09-18 22:20:17 +0300

    custom font & SplashScreen when loading

--- android-app/app/_layout.tsx
+++ android-app/app/_layout.tsx
@@ -1,11 +1,36 @@
-import { Stack, Tabs } from "expo-router";
+import { Stack, SplashScreen } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Colors } from "../shared/tokens";
 import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function  RootLayout() {
 
     const insets = useSafeAreaInsets();
+
+    const [loaded, error] = useFonts({
+		FiraSans: require('../assets/fonts/FiraSans-Regular.ttf'),
+		FiraSansSemiBold: require('../assets/fonts/FiraSans-SemiBold.ttf'),
+	});
+
+    useEffect(() => {
+		if (error) {
+			throw error;
+		}
+	}, [error]);
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null;
+	}
 
     return <SafeAreaProvider>
         <StatusBar style='light' />
