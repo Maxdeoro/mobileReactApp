aef348cd4546cbc86d34feb43c014d2ce49d4776
Maxdeoro <maxdeoro@gmail.com>
2025-09-29 21:47:21 +0300

    first jotaiAtom(user profile), downgrade node to 18.18.0

--- android-app/app/(app)/index.tsx
+++ android-app/app/(app)/index.tsx
@@ -1,8 +1,13 @@
 import { View, Text } from "react-native";
 import { Colors } from "../../shared/tokens";
+import { useAtom } from "jotai";
+import { profileAtom } from "../../entities/user/model/user.state";
+
+const [profile] = useAtom(profileAtom);
 
 export default function MyCourses() {
     return <View>
-        <Text style={{color: Colors.white}}>MY COURSES</Text>
+        <Text>{profile.profile?.name}</Text>
+        {/* <Text style={{color: Colors.white}}>MY COURSES</Text> */}
     </View>;
 };
---
+++ android-app/entities/user/model/user.state.ts
@@ -0,0 +1,19 @@
+import { atom } from "jotai";
+import { User } from "./user.model";
+
+export const profileAtom = atom<UserState>({
+    // initial state
+    profile: {
+        id: 1,
+        name: 'John',
+        surname: 'Snow',
+    },
+    isLoading: false,
+    error: null,
+});
+
+export interface UserState {
+    profile: User | null;
+    isLoading: boolean;
+    error: string | null;
+};
