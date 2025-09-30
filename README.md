c46300ce8dad3f8cf892d163a6783ef2a9c47421
Maxdeoro <maxdeoro@gmail.com>
2025-09-30 22:17:34 +0300

    install @react-native-async-storage

--- README.md
+++
@@ -1,2 +0,0 @@
-# mobileReactApp
-Mobile application created with using React Native, Expo cli typescript and Feature Sliced Design. To start project npx create-expo-app -t expo-template-blank-typescript.
--- android-app/app/(app)/index.tsx
+++ android-app/app/(app)/index.tsx
@@ -2,12 +2,20 @@ import { View, Text } from "react-native";
 import { Colors } from "../../shared/tokens";
 import { useAtom } from "jotai";
 import { profileAtom } from "../../entities/user/model/user.state";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from "react";
 
 const [profile] = useAtom(profileAtom);
+
+useEffect(() => {
+    AsyncStorage.setItem('demo', 'test').then(async () => {
+        console.log(await AsyncStorage.getAllKeys());
+        console.log(await AsyncStorage.getItem('demo'));
+    });
+}, []);
 
 export default function MyCourses() {
     return <View>
         <Text>{profile.profile?.name}</Text>
-        {/* <Text style={{color: Colors.white}}>MY COURSES</Text> */}
     </View>;
 };
