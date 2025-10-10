0cff87e6bd0539a2a714fed416808e4937419c15
Maxdeoro <maxdeoro@gmail.com>
2025-10-10 23:27:46 +0300

    implement logout from app

--- android-app/app/(app)/index.tsx
+++ android-app/app/(app)/index.tsx
@@ -1,37 +1,34 @@
 import { View, Text } from "react-native";
-import { useAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 import { profileAtom } from "../../entities/user/model/user.state";
 import { useEffect } from "react";
 import axios from "axios";
 import { API } from "../../entities/auth/api/api";
 import { AuthResponse } from "../../entities/auth/model/auth.interfaces";
-import { loginAtom } from "../../entities/auth/model/auth.state";
-
-// const login = async () => {
-//     const {data} = await axios.post<AuthResponse>(API.login, { 
-//         email: 'vasia@pupkin.ru',
-//         password: '12345678',
-//     });
-//     console.log(data);
-// };
-
-// useEffect(() => {
-//     login();
-// }, []);
+import { loginAtom, logoutAtom } from "../../entities/auth/model/auth.state";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function MyCourses() {
-    // const [profile] = useAtom(profileAtom);
-    const [auth, login] = useAtom(loginAtom);
+    // const [auth, login] = useAtom(loginAtom);
+    const logout = useSetAtom(logoutAtom);
+
+    // useEffect(() => {
+    //     login({
+    //         email: 'vasia@pupkin.ru',
+    //         password: '12345678',
+    //     });
+    // }, []);
 
     useEffect(() => {
-        login({
-            email: 'vasia@pupkin.ru',
-            password: '12345678',
+        logout();
+        AsyncStorage.getItem('auth').then((data) => {
+            console.log(data);
         });
     }, []);
 
     return <View>
-        <Text>{auth?.access_token}</Text>
+        {/* <Text>{auth?.access_token}</Text> */}
+        <Text>INDEX</Text>
     </View>;
 };
 
--- android-app/entities/auth/model/auth.state.ts
+++ android-app/entities/auth/model/auth.state.ts
@@ -7,11 +7,13 @@ import { API } from "../api/api";
 
 const storage = createJSONStorage<AuthState>(() => AsyncStorage);
 
-export const authAtom = atomWithStorage<AuthState>('auth', {
+const INITIAL_STATE = {
     access_token: null,
     isLoading: false,
     error: null,
-}, storage);
+};
+
+export const authAtom = atomWithStorage<AuthState>('auth', INITIAL_STATE, storage);
 
 export const loginAtom = atom((get) => get(authAtom), 
 async (_get, set, {email, password}: LoginRequest) => {
@@ -39,6 +41,10 @@ async (_get, set, {email, password}: LoginRequest) => {
             });
         }
     }
+});
+
+export const logoutAtom = atom(null, (_get,set) => {
+    set(authAtom, INITIAL_STATE);
 });
 
 export interface AuthState {
