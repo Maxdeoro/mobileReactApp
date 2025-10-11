4d3aafe244f775af427fb6185c67d9d8e0d573d0
Maxdeoro <maxdeoro@gmail.com>
2025-10-11 22:44:45 +0300

    redirect to login page with Redirect-component

--- android-app/app/(app)/index.tsx
+++ android-app/app/(app)/index.tsx
@@ -1,21 +1,21 @@
 import { View, Text } from "react-native";
-import { useAtomValue } from "jotai";
-import { useEffect } from "react";
-import { authAtom } from "../../entities/auth/model/auth.state";
-import { router, useRootNavigationState } from "expo-router";
+// import { useAtomValue } from "jotai";
+// import { useEffect } from "react";
+// import { authAtom } from "../../entities/auth/model/auth.state";
+// import { router, useRootNavigationState } from "expo-router";
 
 export default function MyCourses() {
 
-    const {access_token} = useAtomValue(authAtom);
-    const state = useRootNavigationState();
+    // const {access_token} = useAtomValue(authAtom);
+    // const state = useRootNavigationState();
 
-    useEffect(() => {
-        if(!state?.key) return;
-        
-        if(!access_token){
-            router.replace('/login');
-        }
-    }, [access_token, state]);
+    // useEffect(() => {
+    //     if(!state?.key) return;
+
+    //     if(!access_token){
+    //         router.replace('/login');
+    //     }
+    // }, [access_token, state]);
 
     return <View>
         <Text>INDEX</Text>
