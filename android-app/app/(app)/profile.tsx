fc63b84b5c4392eb3d64e6d150b0070e832bed3b
Maxdeoro <maxdeoro@gmail.com>
2025-10-22 23:03:12 +0300

    permissions to access the phone camera

--- android-app/app/(app)/profile.tsx
+++ android-app/app/(app)/profile.tsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 import { View, Text } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { launchCameraAsync, MediaTypeOptions, useCameraPermissions,
+    PermissionStatus } from 'expo-image-picker';
 import { Button } from '../../shared/button/Button';
 
 export default function Profile() {
 
     const [image, setImage] = useState(null);
+    const [cameraPermissions, requestCameraPermission] = useCameraPermissions();
+
+    const verifyCameraPermissions = async () => {
+        if(cameraPermissions?.status === PermissionStatus.UNDERTERMINED) {
+            const response = await requestCameraPermission();
+            return response.granted;
+        }
+        if(cameraPermissions?.status === PermissionStatus.DENIED) {
+            Alert.alert('Insufficient permissions to access the camera');
+            return false;
+        }
+        return true;
+    };
 
     const pickAvatar = async () => {
-        const result = await ImagePicker.launchCameraAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        const isPermissionGranted = await verifyCameraPermissions();
+
+        if(!isPermissionGranted) {
+            return;
+        }
+
+        const result = await launchCameraAsync({
+            mediaTypes: MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [1, 1],
             quality: 0.5,
