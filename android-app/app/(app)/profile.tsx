0f5f58933449de52e852ab0ca6af909e266c5c4a
Maxdeoro <maxdeoro@gmail.com>
2025-10-23 23:05:33 +0300

    pick avatar from image gallery

--- android-app/app/(app)/profile.tsx
+++ android-app/app/(app)/profile.tsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert, Image } from 'react-native';
 import { launchCameraAsync, MediaTypeOptions, useCameraPermissions,
-    PermissionStatus } from 'expo-image-picker';
+    PermissionStatus, launchImageLibraryAsync, useMediaLibraryPermissions } from 'expo-image-picker';
 import { Button } from '../../shared/button/Button';
 
 export default function Profile() {
 
-    const [image, setImage] = useState(null);
+    const [image, setImage] = useState<string | null>(null);
     const [cameraPermissions, requestCameraPermission] = useCameraPermissions();
+    const [libraryPermissions, requestLibraryPermission] = useMediaLibraryPermissions();
 
     const verifyCameraPermissions = async () => {
-        if(cameraPermissions?.status === PermissionStatus.UNDERTERMINED) {
+        if(cameraPermissions?.status === PermissionStatus.UNDETERMINED) {
             const response = await requestCameraPermission();
             return response.granted;
         }
@@ -21,7 +22,19 @@ export default function Profile() {
         return true;
     };
 
-    const pickAvatar = async () => {
+    const verifyMediaPermissions = async () => {
+        if(libraryPermissions?.status === PermissionStatus.UNDETERMINED) {
+            const response = await requestLibraryPermission();
+            return response.granted;
+        }
+        if(libraryPermissions?.status === PermissionStatus.DENIED) {
+            Alert.alert('Insufficient permissions to access the gallery');
+            return false;
+        }
+        return true;
+    };
+
+    const captureAvatar = async () => {
         const isPermissionGranted = await verifyCameraPermissions();
 
         if(!isPermissionGranted) {
@@ -34,13 +47,43 @@ export default function Profile() {
             aspect: [1, 1],
             quality: 0.5,
         });
+
+        if(!result.assets?.length) {
+            return;
+        }
+        setImage(result.assets[0].uri);
+    };
+
+    const pickAvatar = async () => {
+        const isPermissionGranted = await verifyMediaPermissions();
+
+        if(!isPermissionGranted) {
+            return;
+        }
+
+        const result = await launchImageLibraryAsync({
+            mediaTypes: MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 0.5,
+        });
+
+        if(!result.assets?.length) {
+            return;
+        }
         console.log(result);
+        setImage(result.assets[0].uri);
     };
 
     return (
         <View>
             <Text>Profile</Text>
-            <Button text='Set photo'  onPress={pickAvatar}/>
+            <Button text='Take photo from camera'  onPress={captureAvatar}/>
+            <Button text='Pick image from gallery' onPress={pickAvatar}/>
+            {image && <Image source={{uri: image,
+                width: 100,
+                height: 100,
+            }}/>}
         </View>
     );
 };
