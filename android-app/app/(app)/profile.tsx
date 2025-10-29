cf5058c439916da40bf765cc395e60766a332b50
Maxdeoro <maxdeoro@gmail.com>
2025-10-29 22:31:52 +0300

    share API

--- android-app/app/(app)/profile.tsx
+++ android-app/app/(app)/profile.tsx
@@ -6,11 +6,22 @@ import { Avatar } from '../../entities/user/ui/avatar/Avatar';
 import { updateProfileAtom } from '../../entities/user/model/user.state';
 import { Button } from '../../shared/button/Button';
 import { useAtom } from 'jotai';
+import * as Sharing from 'expo-sharing';
 
 export default function Profile() {
 
     const [image, setImage] = useState<string | null>(null);
     const [profile, updateProfile] = useAtom(updateProfileAtom);
+
+    const shareProfile = async () => {
+        const isSharingAvailable = await Sharing.isAvailableAsync();
+        if(!isSharingAvailable) {
+            return;
+        }
+        await Sharing.shareAsync('https://purpleschool.ru', {
+            dialogTitle: 'Share Profile',
+        });
+    };
 
     const submitProfile = () => {
         if(!image) {
@@ -32,6 +43,7 @@ export default function Profile() {
                 <ImageUploader onUpload={setImage} onError={(e) => console.log(e)}/>
             </View>
             <Button text="Save" onPress={submitProfile}/>
+            <Button text='Share' onPress={shareProfile}/>
         </View>
     );
 };
