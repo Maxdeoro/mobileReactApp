ec1dbe63b54306648f0ce0a46c52d934cb5f20f9
Maxdeoro <maxdeoro@gmail.com>
2025-11-19 21:37:05 +0300

    environmental constants

--- android-app/shared/api.ts
+++ android-app/shared/api.ts
@@ -1,4 +1,4 @@
-export const PREFIX = 'https://purpleschool.ru/api-v2';
+export const PREFIX = `${process.env.EXPO_PUBLIC_DOMAIN}/api-v2`;
 
 export const API = {
     login: `${PREFIX}/auth/login`,
